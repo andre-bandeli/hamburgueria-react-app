4da2c49c624c99f9f1a039832dbbe9aaf6266d73
andre-bandeli <andre.lbandeli@gmail.com>
2022-10-08 02:37:43 -0300

    feat: funcionamento

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from "../src/components/Nav/Navbar"
 import Header from './components/header/Header';
 import CardsPromocao from './components/cards-promocao/CardsPromocao';
+import CardsFuncionamento from './components/cards-funcionamento/CardsFuncionamento';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Header/>
-      {/* <CardsPromocao/> */}
+      <CardsPromocao/>
+      <CardsFuncionamento/>
     </div>
   );
 }
---
+++ src/components/cards-funcionamento/CardsFuncionamento.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './cardsFuncionamento.scss'
+
+import icon from '../cards-funcionamento/relogio.png'
+export default function CardsFuncionamento() {
+  return (
+    <div className='cards-funcionamento'>
+
+        <div className="container">
+            <div className="col-6-red">
+                <div className="img">
+                    <img src={icon} alt="" />
+                </div>
+                <div className="text">
+                  <h2>HORÁRIO DE FUNCIONAMENTO</h2>
+                  <h3>Segunda-feira a sexta-feira: 17h00 - 23h00</h3>
+                  <h3>Sabado a Domíngo: 18h30 - 23h00</h3>
+
+                </div>
+            </div>
+            <div className="col-6">
+                <h3>Não esqueça de marcar a gente no Instagram:</h3>
+                <h2>#EMPIREBURGUER </h2>
+            </div>
+        </div>
+
+    </div>
+  )
+}
